6b3e261dae5929b9493f5e029e6381dde2652c51
thayark <rkt12357@gmail.com>
2024-02-14 16:14:11 +0530

    updated

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
 // require("./bomb-grenade")
-const os=require('os')
-const path =require("path")
-const name=require("./one")
+// const os=require('os')
+// const path =require("path")
+// const name=require("./one")
 
-const user=os.userInfo()
-console.log(user)
-console.log(`the system uptime is ${os.uptime()} seconds`)
+// const user=os.userInfo()
+// console.log(user)
+// console.log(`the system uptime is ${os.uptime()} seconds`)
 
-const currentOS ={
-    name:os.type(),
-    release:os.release(),
-    totalMem:os.totalmem(),
-    freeMem:os.freemem(),
-}
+// const currentOS ={
+//     name:os.type(),
+//     release:os.release(),
+//     totalMem:os.totalmem(),
+//     freeMem:os.freemem(),
+// }
 
-console.log(currentOS)
-
-
-const testpath= path.join('/content','subfolder','test.txt')
-console.log(testpath)
+// console.log(currentOS)
 
 
-const base=path.basename(testpath)
-console.log(base)
+// const testpath= path.join('/content','subfolder','test.txt')
+// console.log(testpath)
 
-const abs=path.resolve(__dirname,'test.txt')
-console.log(abs)
 
-console.log(name.adam);
+// const base=path.basename(testpath)
+// console.log(base)
+
+// const abs=path.resolve(__dirname,'test.txt')
+// console.log(abs)
+
+// console.log(name.adam);
+
+const _=require('lodash')
+
+const items=[1,[2,[3,[4]]]]
+const nitems=_.flattenDeep(items)
+console.log(nitems)
+console.log("hello people")
